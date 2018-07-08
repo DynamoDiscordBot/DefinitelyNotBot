90434ee6f65d544b8723c905c5eac257b7528e6b
DynamoDiscordBot <40926358+DynamoDiscordBot@users.noreply.github.com>
2018-07-08 09:06:40 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,21 +1,24 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const prefix = "l!";
 
 client.on('ready', () => {
     console.log('I am ready!');
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.channel.send('PONG!');
-  	}
+
+    if (message.author.bot) return;
+
+    const message = prefix + message.content;
+
+    if (command === 'ping') {
+        message.channel.send(Math.floor(client.ping));
+      }
+    if (command === 'bing') {
+        message.reply('BONG!');
+      }
 });
 
-client.on('message', message => {
-    if (message.content === 'bing') {
-    	message.reply('BONG!');
-  	}
-});
-
-// THIS  MUST  BE  THIS  WAY
+// THIS  has to be here do not change it
 client.login(process.env.BOT_TOKEN);
